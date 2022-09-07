bbf569498928307d71966cb933d8bd94cccc9269
sanitwasnik <84314505+sanitwasnik@users.noreply.github.com>
2022-09-07 23:00:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+
+
+});
+
+app.post("/", function(req, res){
+
+  const query = req.body.cityName;
+  const apiKey = "2240459ffa224a8d7b36886bc6590201";
+  const units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      const temper = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+
+      const imageURL = " http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      res.write("<p>The weather Description is " + weatherDescription + "<p>");
+      res.write("<h1>The Temperature of " + query + " is" + temper + "Degere celcius</h1>");
+      res.write("<img src=" + imageURL + ">");
+      res.send()
+
+
+    })
+  })
+})
+
+
+
+app.listen(3000, function(){
+  console.log("server is running at port 3000");
+})
